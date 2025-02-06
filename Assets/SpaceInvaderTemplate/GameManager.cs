9e6363fb9b2dd6429af16259a0e9eb3df96c938b
AeroPorc <hugopv.smp@gmail.com>
2025-02-06 22:07:51 +0100

    Update Activated

--- Assets/SpaceInvaderTemplate/GameManager.cs
+++ Assets/SpaceInvaderTemplate/GameManager.cs
@@ -18,6 +18,7 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private GameObject _camera;
     [SerializeField] private GameObject _gameOverPrefab;
+    private bool _activated = false;
 
     void Awake()
     {
@@ -76,11 +77,15 @@ public class GameManager : MonoBehaviour
     public void PlayGameOver()
     {
         Debug.Log("Game Over");
-        GameObject gameOverInstance = GameObject.Instantiate(_gameOverPrefab);
-        Vector3 position = _camera.transform.position + _camera.transform.forward * 1f;
-        position.z = -9f;
-        gameOverInstance.transform.position = position;
-        _camera.GetComponent<CameraShake>().shakecamera();
+        if(_activated)
+        {
+            GameObject gameOverInstance = GameObject.Instantiate(_gameOverPrefab);
+            Vector3 position = _camera.transform.position + _camera.transform.forward * 1f;
+            position.z = -9f;
+            gameOverInstance.transform.position = position;
+            _camera.GetComponent<CameraShake>().shakecamera();
+        }
+
         StartCoroutine(WaitAndRestart(3f));
     }
 
@@ -104,6 +109,10 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _activated = !_activated;
+        }
+        if(_activated)
         {
             _camera.GetComponent<Volume>().weight = 1f;
         }
